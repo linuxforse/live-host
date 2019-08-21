6ef7d4922b0e5ae5eeeefbcba634e4ffe98ef33c
linuxforse <linuxforse@gmail.com>
2019-08-21 14:18:15 +0300

    Update lhost.sh
    Добавлена проверка хоста с использованием TCP протокола.

--- lhost.sh
+++ lhost.sh
@@ -16,17 +16,25 @@ if ping -c1 "$mhost" &> /dev/null
 then
 #Вместо "$HOME/*/live-host/" укажите свой (правильный) путь к иконкам. Без иконок скрипт тоже будет правильно работать.
 notify-send -i $HOME/*/live-host/img/gdu-smart-healthy.png "Ваш хост $mhost" "Отвечает на запросы"
+elif (exec 3</dev/tcp/"$mhost"/80) 2>/dev/null
+then
+notify-send -i $HOME/*/live-host/img/gdu-smart-healthy.png "Ваш хост $mhost" "Отвечает на запросы111"
+(exec 3<&~) 2>/dev/null
 else
 notify-send -i $HOME/*/live-host/img/gdu-smart-failing.png "Ваш хост $mhost" "Не отвечает на запросы"
 fi
 
-
+unset mhost
 }
 #Проверяем наличие доступа к сети Интернет
 if !(dig google.com | grep REFUSED &>/dev/null)
 then
 
 loadres $1
+elif (exec 4</dev/tcp/google.com/80) 2>/dev/null
+then
+loadres $1
+(exec 4<&~) 2>/dev/null
 else
 notify-send -i $HOME/*/live-host/img/no-internet-pc.png "Вероятно отсутствует подключение" "К Иинтеренету"
 fi
